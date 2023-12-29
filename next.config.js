a093a1394c19edf7c65652214fe91100a7341af2
JEDIBASIL <jedidiahbasil@gmail.com>
2023-12-29 06:05:24 +0100

    edited config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.(pdf)$/,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        fallback: 'file-loader',
-                        publicPath: '/_next/static/',
-                        outputPath: 'static/',
-                        name: '[name].[hash].[ext]',
-                    },
-                },
-            ],
-        });
 
-        return config;
-    },
 }
 
 module.exports = nextConfig
